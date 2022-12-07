33a285e06e32096ff2ade9efc57d443054b8db98
daved09 <17879596+daved09@users.noreply.github.com>
2022-12-07 09:25:51 +0100

    initial commit

---
+++ src/main/java/de/david/QuizShowCreator/QuizShowCreatorApplication.java
@@ -0,0 +1,13 @@
+package de.david.QuizShowCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizShowCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizShowCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/david/QuizShowCreator/QuizShowCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.david.QuizShowCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizShowCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
